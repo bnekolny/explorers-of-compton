0770947c4874f7f6f5b0bcb5f5c6070ca0f903c1
Brett Nekolny <brett.nekolny@mapmyfitness.com>
2014-06-24 09:48:19 -0600

    Making some changes and working with some APIs.

--- app.js
+++ app.js
@@ -8,16 +8,58 @@ var application_root = __dirname,
 var app = express();
 
 // Database
+mongo_connection = null;
+app.configure('production', function () {
+    mongo_connection = 'mongodb://tictactoe:bc64c5cb9171f5f562697926ba32d8ea@kahana.mongohq.com:10062/app26528232';
+});
+
+app.configure('development', function () {
+    //mongo_connection = 'mongodb://tictactoe:bc64c5cb9171f5f562697926ba32d8ea@localhost:27017';
+    mongo_connection = 'mongodb://localhost:27017';
+});
 
 var mongo = null;
-MongoClient.connect('mongodb://tictactoe:bc64c5cb9171f5f562697926ba32d8ea@kahana.mongohq.com:10062/app26528232', function(err, db) {
+MongoClient.connect(mongo_connection, function(err, db) {
     mongo = db;
+    if (err) {
+        console.error(err);
+    }
 })
 
-
+// Requests
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
 });
+
+
+getTicTacToeGames = function(callback) {
+    collection = mongo.collection('tictactoe');
+    collection.find().toArray(function(err, items) {
+        if (err) console.error(err.message);
+        callback(items);
+    });
+};
+
+app.get('/tictactoe', function(request, response) {
+    games = getTicTacToeGames(function(games) {
+        // If there aren't any games, create one
+        if (games.length == 0) {
+            collection = mongo.collection('tictactoe');
+            collection.insert({}, function(err, objects) {
+                if (err) console.warn(err.message);
+            });
+        }
+    });
+    response.sendfile(application_root + '/tictactoe.html');
+});
+
+
+app.get('/tictactoe/api', function (request, response) {
+    getTicTacToeGames(function(games) {
+        response.send({'games': games});
+    });
+});
+
 
 app.configure(function() {
     app.use('/', express.static(__dirname + '/'));
@@ -28,3 +70,5 @@ app.configure(function() {
 });
 
 app.listen(port);
+
+//mongo.close()
