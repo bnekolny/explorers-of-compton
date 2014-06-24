b133f2af70f999d52b2ec5d59d2bc72b9043bb76
Brett Nekolny <brett.nekolny@mapmyfitness.com>
2014-06-23 21:30:56 -0600

    Making the app.js more readible.

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ var express = require('express');
 var port = process.env.PORT || 5000;
 var app = express.createServer();
 
+
+
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
-}).configure(function() {
+});
+
+app.configure(function() {
     app.use('/', express.static(__dirname + '/'));
     app.use('/images', express.static(__dirname + '/images'));
     app.use('/javascripts', express.static(__dirname + '/javascripts'));
     app.use('/stylesheets', express.static(__dirname + '/stylesheets'));
     app.use('/sounds', express.static(__dirname + '/sounds'));
-}).listen(port);
+});
+
+app.listen(port);
