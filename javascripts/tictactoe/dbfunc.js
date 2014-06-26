c2ebccef5a1fd41de6b709c1409817803cfaa0d7
Jeff Hardy <jeffthardy@Jeff-Hardys-MacBook-Pro.local>
2014-06-25 22:47:08 -0600

    switched add players to jquery for kicks

--- javascripts/tictactoe/dbfunc.js
+++ javascripts/tictactoe/dbfunc.js
@@ -1,20 +1,36 @@
 
 
-$(document).ready( function(){
+// $(document).ready( function(){
 
-	var myButton = document.getElementById("create_game_btn");
-	var myForm = document.getElementById("create_game_form");
+// 	var myButton = document.getElementById("create_game_btn");
+// 	var myForm = document.getElementById("create_game_form");
 
-	function createGame(form) {
+// 	function createGame(form) {
+// 		$.post('/tictactoe/api', 
+// 		       { player1 : form.player1.value,
+// 		         player2 : form.player2.value } );
+// 	};
+
+// 	function clickedCreateGame(){
+
+// 		createGame(myForm);
+// 	};
+
+// 	myButton.addEventListener('click', clickedCreateGame,false);
+// });
+
+
+$(document).ready(function(){
+
+	function createGame(player1Name, player2Name) {
 		$.post('/tictactoe/api', 
-		       { player1 : form.player1.value,
-		         player2 : form.player2.value } );
+		       { player1 : player1Name,
+		         player2 : player2Name} );
 	};
 
-	function clickedCreateGame(){
 
-		createGame(myForm);
-	};
+	$("#create_game_btn").click(function(){
+		createGame($("#create_game_form input[name=player1]").val(),$("#create_game_form input[name=player2]").val());
+	});
 
-	myButton.addEventListener('click', clickedCreateGame,false);
 });
