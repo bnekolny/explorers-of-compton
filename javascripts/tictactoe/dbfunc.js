7a41c4113fc659ff088e6c0b1046d3b2eb8276f2
Jeff Hardy <jeffthardy@Jeff-Hardys-MacBook-Pro.local>
2014-06-28 22:20:23 -0600

    adding some cool database functions.  Lists games and will filter based on login name

--- javascripts/tictactoe/dbfunc.js
+++ javascripts/tictactoe/dbfunc.js
@@ -24,7 +24,8 @@ $(document).ready(function(){
 
 	// On load get the game database and display list of games
 
-
+	var webviewPlayerName = "";
+	var webviewGameIDSelection;
 
 
 
@@ -38,4 +39,53 @@ $(document).ready(function(){
 		createGame($("#create_game_form input[name=player1]").val(),$("#create_game_form input[name=player2]").val());
 	});
 
+
+
+	// Create a game on button click
+	function listGames() {
+		$.getJSON('/tictactoe/api', function(data){
+			var items = [];
+			for(var entry in data){
+				var gameDatabase = data[entry];
+				for(var gameEntry in gameDatabase){
+					var gameInfo = gameDatabase[gameEntry];
+					// Only add games to display if player1 and player2 are defined.
+					// eventually check agains my player name and if the games are complete
+					if(webviewPlayerName == ""){
+						if(gameInfo["player1"] && gameInfo["player2"]){
+							$("<div class='gameSelect' id='" + gameInfo["_id"] + "'>" + gameInfo["player1"] + " vs " +  gameInfo["player2"] + "</div>" ).appendTo("#listOfGames");
+						}
+					}
+					else{
+						if((gameInfo["player1"] && gameInfo["player2"]) && ((gameInfo["player1"] == webviewPlayerName)||(gameInfo["player2"] == webviewPlayerName))){
+							$("<div class='gameSelect' id='" + gameInfo["_id"] + "'>" + gameInfo["player1"] + " vs " +  gameInfo["player2"] + "</div>" ).appendTo("#listOfGames");
+						}
+					}
+
+				}
+			}
+
+		});
+	};
+		listGames();
+
+	var ok = function selectGameFromList(){
+
+	}
+
+    $("#listOfGames").on("click",".gameSelect",function(event) {
+		webviewGameIDSelection = event.target.id;
+		$("div").removeClass("highlight");
+		$("#" + webviewGameIDSelection).addClass("highlight");
+    });
+
+    $("#login_form").change(function(){
+    	webviewPlayerName = $("#login_form input[name=playerName]").val();
+    	$(".gameSelect").remove();
+    	listGames();
+
+    });
+
 });
+
+
